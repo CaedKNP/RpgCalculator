396d246e5b6202cf2252cb3d777593f1c992b8fc
Caed <32883053+CaedKNP@users.noreply.github.com>
2022-11-04 21:50:39 +0100

    colorVisualizer and ressist

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ var reset = document.getElementById("resetAll");
 
 var dmgButtons = document.getElementsByClassName("dmg");
 var damageField = document.getElementById("dmgSum");
+var ressButtons = document.getElementsByClassName("ress");
+var ressField = document.getElementById("ressSum");
 
 var headButton = document.getElementById("HeadB");
 var forntButton = document.getElementById("FrontB");
@@ -35,10 +37,36 @@ var barriers = {
 };
 
 var dmgSum = 0;
+var ressSum = 0;
+var limbB = 0;
+var limbA = 0;
+
+var color1 = {
+    red: 130,
+    green: 0,
+    blue: 0
+};
+var color2 = {
+    red: 130,
+    green: 95,
+    blue: 0
+};
+
+var color3 = {
+    red: 0,
+    green: 0,
+    blue: 0
+};
+
+var color4 = {
+    red: 145,
+    green: 145,
+    blue: 145
+};
 
 function divideStats(){
-    var dB = Math.round(barrier/7);
-    var dA = Math.round(armor/7);
+    var dB = limbB =Math.round(barrier/7);
+    var dA = limbA = Math.round(armor/7);
     
     for(var key in armors)
         armors[key] = dA;
@@ -60,6 +88,12 @@ function addDmg() {
     damageField.innerHTML = dmgSum;
 };
 
+function addRess() {
+    var value = parseInt(this.getAttribute("id"));
+    ressSum += value;
+    ressField.innerHTML = ressSum;
+};
+
 function updateVisual(){
     headButton.innerHTML = barriers.Head;
     forntButton.innerHTML = barriers.Front;
@@ -70,39 +104,71 @@ function updateVisual(){
     rLegButton.innerHTML = barriers.RightLeg;
 }
 
+function VisualizeDmg(limb, button)
+{
+    if(barriers[limb] > 0)
+    {
+        barriers[limb] -= calcDmg();
+        button.innerHTML = barriers[limb];
+        button.style.backgroundColor = calcGradient1(barriers[limb]/limbB);
+    }
+    else if(armors[limb] > 0)
+    {
+        armors[limb] -= calcDmg();
+        button.innerHTML = armors[limb];
+        button.style.backgroundColor = calcGradient2(armors[limb]/limbA);
+    }
+}
+
+function calcDmg()
+{
+    return dmgSum - ((dmgSum*ressSum)/100);
+}
+
+function calcGradient1(percent)
+{
+    var resultRed = color1.red + percent * (color2.red - color1.red);
+    var resultGreen = color1.green + percent * (color2.green - color1.green);
+    var resultBlue = color1.blue + percent * (color2.blue - color1.blue);
+
+    return 'rgb(' + resultRed + ',' + resultGreen + ',' + resultBlue + ')'; 
+}
+
+function calcGradient2(percent)
+{
+    var resultRed = color3.red + percent * (color4.red - color3.red);
+    var resultGreen = color3.green + percent * (color4.green - color3.green);
+    var resultBlue = color3.blue + percent * (color4.blue - color3.blue);
+
+    return 'rgb(' + resultRed + ',' + resultGreen + ',' + resultBlue + ')'; 
+}
+
 //execute on start
 updateVisual()
-headButton.style.backgroundColor = 'rgb(204, 197, 8)';
+//headButton.style.backgroundColor = 'rgb(204, 197, 8)';
 
 divideStats();
 reset.addEventListener("click", resetClick);
 headButton.addEventListener("click", function() {
-    barriers.Head -= dmgSum;
-    headButton.innerHTML = barriers.Head;
+    VisualizeDmg('Head', headButton);
 });
 forntButton.addEventListener("click", function() {
-    barriers.Front -= dmgSum;
-    forntButton.innerHTML = barriers.Front;
+    VisualizeDmg('Front', forntButton);
 });
 backButton.addEventListener("click", function() {
-    barriers.Back -= dmgSum;
-    backButton.innerHTML = barriers.Back;
+    VisualizeDmg('Back', backButton);
 });
 lHandButton.addEventListener("click", function() {
-    barriers.LeftHand -= dmgSum;
-    lHandButton.innerHTML = barriers.LeftHand;
+    VisualizeDmg('LeftHand', lHandButton);
 });
 rHandButton.addEventListener("click", function() {
-    barriers.RightHand -= dmgSum;
-    rHandButton.innerHTML = barriers.RightHand;
+    VisualizeDmg('RightHand', rHandButton);
 });
 lLegButton.addEventListener("click", function() {
-    barriers.LeftLeg -= dmgSum;
-    lLegButton.innerHTML = barriers.LeftLeg;
+    VisualizeDmg('LeftLeg', lLegButton);
 });
 rLegButton.addEventListener("click", function() {
-    barriers.RightLeg -= dmgSum;
-    rLegButton.innerHTML = barriers.RightLeg;
+    VisualizeDmg('RightLeg', rLegButton);
 });
 
 damageField.addEventListener('click', function(){
@@ -112,4 +178,13 @@ damageField.addEventListener('click', function(){
 
 for (var i = 0; i < dmgButtons.length; i++) {
     dmgButtons[i].addEventListener('click', addDmg, false);
+}
+
+ressField.addEventListener('click', function(){
+    ressSum = 0;
+    ressField.innerHTML = ressSum;
+});
+
+for (var i = 0; i < ressButtons.length; i++) {
+    ressButtons[i].addEventListener('click', addRess, false);
 }
